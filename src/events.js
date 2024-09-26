3418bb058900867fb96b56dee638ca9a767c398b
Mattéo Delabre <git.matteo@delab.re>
2024-09-26 18:23:52 -0400

    Cache responses from Discord API

--- src/events.js
+++ src/events.js
@@ -1,26 +1,93 @@
 const { GuildScheduledEventEntityType } = require("discord.js");
+const { open, mkdir } = require("node:fs/promises");
+
+const cacheFolderPath = "cache/";
+const cacheFilePath = cacheFolderPath + "events.json";
+
+const cacheTime = 1000 * 60 * 5; // 5 minutes
+let eventsCache = {
+    updates: {},
+    events: {},
+};
+
+// Read cached events from disk
+(async () => {
+    try {
+        await mkdir(cacheFolderPath);
+    } catch (err) {
+        if (err.code != "EEXIST") {
+            throw err;
+        }
+    }
+
+    let cacheFile;
+
+    try {
+        cacheFile = await open(cacheFilePath, "r");
+        eventsCache = JSON.parse(await cacheFile.readFile());
+    } catch (err) {
+        if (err.code != "ENOENT") {
+            throw err;
+        }
+    } finally {
+        await cacheFile?.close();
+    }
+})();
 
 const fetchEvents = async (guild) => {
-    const events = await guild.scheduledEvents.fetch();
-    return events
-        .filter(
-            (event) =>
-                event.entityType == GuildScheduledEventEntityType.External,
-        )
-        .map((event) => ({
-            uid: event.id,
-            name: event.name,
-            description: event.description,
-            location: event.entityMetadata.location,
-            start: event.scheduledStartAt,
-            end: event.scheduledEndAt,
-            created: event.createdAt,
-        }));
+    if (
+        !(guild.id in eventsCache.updates) ||
+        !(guild.id in eventsCache.events) ||
+        Date.now() - eventsCache.updates[guild.id] > cacheTime
+    ) {
+        // Retrieve events from the Discord API
+        const rawEvents = await guild.scheduledEvents.fetch();
+        const events = Object.fromEntries(
+            rawEvents
+                .filter(
+                    (event) =>
+                        event.entityType ==
+                        GuildScheduledEventEntityType.External,
+                )
+                .map((event) => [
+                    event.id,
+                    {
+                        uid: event.id,
+                        name: event.name,
+                        description: event.description,
+                        location: event.entityMetadata.location,
+                        start: event.scheduledStartAt.getTime(),
+                        end: event.scheduledEndAt.getTime(),
+                        created: event.createdAt.getTime(),
+                    },
+                ]),
+        );
+
+        // Merge with events in cache
+        eventsCache.updates[guild.id] = Date.now();
+        eventsCache.events[guild.id] = Object.assign(
+            {},
+            eventsCache.events[guild.id],
+            events,
+        );
+
+        // Write new events in cache file
+        let cacheFile;
+
+        try {
+            cacheFile = await open(cacheFilePath, "w");
+            await cacheFile.writeFile(JSON.stringify(eventsCache));
+        } finally {
+            await cacheFile.close();
+        }
+    }
+
+    return eventsCache.events[guild.id];
 };
 
 const dateToICal = (date) => {
     return (
-        date
+        new Date(date)
             .toISOString()
             .replaceAll(":", "")
             .replaceAll("-", "")
@@ -28,7 +95,7 @@ const dateToICal = (date) => {
     );
 };
 
-const wrapICal = (data) => data.replace("\n", "\\r\\n");
+const wrapICal = (data) => data.replace("\n", "\\n");
 
 const generateICal = (name, domain, lang, events) => {
     const lines = [
@@ -40,7 +107,7 @@ const generateICal = (name, domain, lang, events) => {
         "METHOD:PUBLISH",
     ];
 
-    for (let event of events) {
+    for (let event of Object.values(events)) {
         let start = event.start;
         let end = event.end;
 
